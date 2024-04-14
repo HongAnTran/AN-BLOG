b75c7c4e2c52807cacee8af08eab6490b9e74470
AnTran <tranhongankrn.2001@gmail.com>
2024-04-14 22:16:00 +0700

    up code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-import BlogsService from "@/api/client/blogs"
-import { TypographyP } from "@/components/ui/Typographys"
-import routes from "@/consts/routes"
-import Link from "next/link"
+
 import { Suspense } from "react"
 export default async function HomePage() {
   return (
@@ -16,34 +13,15 @@ export default async function HomePage() {
 
 async function BlogList() {
 
-  try {
-    const  blogs  = await BlogsService.getList({ limit: 10 })
-    // const  blogs : any[]  = []
 
-    if (!blogs) {
-      return <></>
-    }
-    return (
+  return (
 
-      <div className=" flex gap-2 flex-wrap justify-between">
+    <div className=" flex gap-2 flex-wrap justify-between">
 
-        {blogs.map((blog) => {
-          return (
-            <Link key={blog.id} href={`${routes.blogs}/${blog.id}`}>
-              <div className=" p-4  w-[200px] h-[200px] bg-black  border  border-white rounded-md  shadow-sm">
-                <TypographyP className=" text-white text-center font-bold">
-                  {blog.id}.{blog.title}
-                </TypographyP>
+      <h1>hello</h1>
+    </div>
 
-              </div>
-            </Link>
-          )
-        })}
-      </div>
+  )
 
-    )
 
-  } catch (error) {
-    throw Error(JSON.stringify(error))
-  }
 }
